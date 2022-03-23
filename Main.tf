47b75a2a204c340ddb30533ca089a547f3387172
epavankumar91 <99183447+epavankumar91@users.noreply.github.com>
2022-03-23 15:28:19 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "webApp" {
+  ami           = "ami-058927a80413afd9e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JenkinsTErraformInstance"
+  }
+}
+
+provider "aws"{
+ region = "ap-south-1"
+}
